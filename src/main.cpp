c515644135d272a91e14f74503f2a697817bf199
nikita <nikita125568@ya.ru>
2024-08-31 13:54:30 -0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -7,40 +7,9 @@
 using namespace trantor;
 #define USE_IPV6 0
 
+std::queue<trantor::TcpConnectionPtr> queue;
+
 void processICAPRequest(const TcpConnectionPtr& connectionPtr, MsgBuffer* buffer) {
-    std::string receivedData(buffer->peek(), buffer->readableBytes());
-    std::cout << "Received ICAP request: " << receivedData << std::endl;
-
-    if (receivedData.find("OPTIONS") != std::string::npos) {
-        std::string response = "ICAP/1.0 200 OK\r\n"
-                               "Methods: RESPMOD REQMOD OPTIONS\r\n"
-                               "Service: ExampleICAPServer\r\n"
-                               "Transfer-Preview: *\r\n"
-                               "Preview: 1024\r\n"
-                               "Max-Connections: 100\r\n"
-                               "Options-TTL: 3600\r\n"
-                               "Date: Mon, 30 Nov 2023 01:01:01 GMT\r\n\r\n";
-        connectionPtr->send(response.data(), response.size());
-    } else if (receivedData.find("REQMOD") != std::string::npos) {
-        // Process REQMOD request here
-        std::string response = "ICAP/1.0 200 OK\r\n"
-                               "Encapsulated: req-hdr=0, null-body=" + std::to_string(receivedData.size()) + "\r\n"
-                               "\r\n";
-        connectionPtr->send(response.data(), response.size());
-    } else if (receivedData.find("RESPMOD") != std::string::npos) {
-        // Process RESPMOD request here
-        std::string response = "ICAP/1.0 200 OK\r\n"
-                               "Encapsulated: res-hdr=0, res-body=" + std::to_string(receivedData.size()) + "\r\n"
-                               "\r\n";
-        connectionPtr->send(response.data(), response.size());
-    } else {
-        std::string response = "ICAP/1.0 400 Bad Request\r\n"
-                               "Date: Mon, 30 Nov 2023 01:01:01 GMT\r\n"
-                               "\r\n";
-        connectionPtr->send(response.data(), response.size());
-    }
-
-    buffer->retrieveAll();
 }
 
 int main() {
